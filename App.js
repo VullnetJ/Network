33d89b20f347cebcb776325e9f591fd1a3d8755e
VullnetJ <v.jata24@gmail.com>
2021-08-29 16:46:48 +0300

    second commit

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [rates, setRates] = useState([]);
+  const [amount, setAmount] = useState(0);
+  const [currency, setCurrency] = useState("");
+  const [result, setResult] = useState(0);
+  
+  useEffect(() => {
+    fetch('https://api.exhangeratesapi.io/latest')
+    .then (response => response.json())
+    .then(responseJson => setRates(responseJson.rates))
+    .catch(error => {
+      Alert.alert(error);
+    })
+  })
+
+  
+
+
+  const rateConversion = () => {
+    const rate = rates[currency];
+    setResults((amount/ rate).toFixed(5))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar hidden={true} />
+      <Text> {result} $</Text>
       <StatusBar style="auto" />
     </View>
   );
